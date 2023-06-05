6ef5c3fa2f76d67d2f1065e98e5a869f8e45e300
unknown <lucas.corradini.2003@gmail.com>
2023-06-05 16:50:38 -0700

    fixing some issues

--- inc/Utilities/Page.class.php
+++ inc/Utilities/Page.class.php
@@ -56,9 +56,7 @@ class Page{
             <td>'.$student->getStudentCourse().'</td>
             <td>'.$student->getStudentEmail().'</td>
             <td>'.$student->getStudentCountry().'</td>
-            <td>
-                <a href="?editForm=edit&stuId= '.$student->getStudentUserName().'" >Edit</a>
-            </td>
+            <td><a href="?editForm=edit&stuId='.$student->getStudentUserName().'">Edit</a></td>
         </tr>
         ';
         return $stuRow;
