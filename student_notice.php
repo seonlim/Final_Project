1332b251637d80a046a1d9cfbeed62a6dd463bd1
lu-corradini <lucas.corradini.2003@gmail.com>
2023-06-07 18:54:33 -0700

    updated some files to the pull request

--- student_notice.php
+++ student_notice.php
@@ -4,7 +4,7 @@ require_once("inc/Utilities/PDOService.class.php");
 require_once("inc/Utilities/DAO/NoticeDAO.class.php");
 require_once("inc/Utilities/NoticeRepository.class.php");
 require_once("inc/Entities/Notice.class.php");
-require_once("inc/Utilities/Page.class.php");
+require_once("inc/Utilities/Notice_Page.class.php");
 
 NoticeDAO::startDb();
 
