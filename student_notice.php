dda31c0d758e423104102d03839dc329b6146199
Hyungsun Lim <bokeshhys@gmail.com>
2023-06-07 18:44:38 -0700

    Add the CSS file

--- student_notice.php
+++ student_notice.php
@@ -8,23 +8,7 @@ require_once("inc/Utilities/Page.class.php");
 
 NoticeDAO::startDb();
 
-
-if (isset($_POST['noticeForm'])) {
-    $newNotice = new Notice();
-    $newNotice->setNoticeTitle($_POST['noticeTitle']);
-    $newNotice->setDescription($_POST['description']);
-    $newNotice->setWriteDate(date('Y-m-d'));
-    $newNotice->setWriter($_POST['writer']);
-
-    NoticeDAO::insertNotice($newNotice);
-    // var_dump(NoticeDAO::getAllNotices());
-    unset($_POST['noticeForm']);
-}
-
-
-
 echo Page::getPageHead();
 echo Page::noticeTable(NoticeDAO::getAllNotices());
-echo Page::newNoticeForm();
 echo Page::getPageFooter();
 ?>
---
+++ teacher_notice.php
@@ -0,0 +1,30 @@
+<?php
+require_once("inc/config.inc.php");
+require_once("inc/Utilities/PDOService.class.php");
+require_once("inc/Utilities/DAO/NoticeDAO.class.php");
+require_once("inc/Utilities/NoticeRepository.class.php");
+require_once("inc/Entities/Notice.class.php");
+require_once("inc/Utilities/Page.class.php");
+
+NoticeDAO::startDb();
+
+
+if (isset($_POST['noticeForm'])) {
+    $newNotice = new Notice();
+    $newNotice->setNoticeTitle($_POST['noticeTitle']);
+    $newNotice->setDescription($_POST['description']);
+    $newNotice->setWriteDate(date('Y-m-d'));
+    $newNotice->setWriter($_POST['writer']);
+
+    NoticeDAO::insertNotice($newNotice);
+    // var_dump(NoticeDAO::getAllNotices());
+    unset($_POST['noticeForm']);
+}
+
+
+
+echo Page::getPageHead();
+echo Page::noticeTable(NoticeDAO::getAllNotices());
+echo Page::newNoticeForm();
+echo Page::getPageFooter();
+?>
